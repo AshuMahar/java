02aa06195de202b68ef00f08b20bcce8cd764d3a
AshuMahar <91270377+AshuMahar@users.noreply.github.com>
2022-04-05 10:55:57 +0530

    Add files via upload

---
+++ Insideclass.java
@@ -0,0 +1,18 @@
+
+public class Insideclass 
+{
+	private int priv_var=1;
+	public int pub_var=2;
+	protected int prot_var=3;
+	int def_var=4;
+	void display()
+	{
+		System.out.println("Private variable [priv_var] is"+priv_var);
+		System.out.println("Public variable [pub_var]is"+pub_var);
+		System.out.println("Protected variable [pro_var]is"+prot_var);
+		System.out.println("Default variable[def_var]is"+ def_var );
+}
+	
+	
+
+}
